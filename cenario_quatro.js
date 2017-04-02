94d4d72123da836de61548ec9d9c53c2834da041
Luan Muniz <luan@luanmuniz.com.br>
2017-04-02 11:44:05 -0300

    Update cenario

--- cenario_quatro.js
+++ cenario_quatro.js
@@ -17,7 +17,6 @@ module.exports = {
 		});
 
 		let arrayToSend = [];
-
 		JSON.parse(apiResult.body).forEach((thisPresente) => {
 			let precoPromocao = (thisPresente.price * 0.7).toFixed(2);
 			let prontos = Math.ceil(thisPresente.price / 4.2).toFixed(0);
@@ -44,32 +43,34 @@ module.exports = {
 		s.endDialog();
 	},
 
-	lancamento(s) {
+	async lancamento(s) {
 		s.send('Os lançamentos do ciclo atual sao esses abaixos');
 
-		helper.sendSlider(s, [{
-			"title": "LANCAMENTO 1",
-			"subtitle": "Você paga R$ 450 em 2x no boleto e pode Vender por até R$ 585!",
-			"images": [
-				{ "url": "https://dummyimage.com/600x300" }
-			],
-			"buttons": [{
-				"type": "openUrl",
-				"title": "Veja mais sobre essa promoção",
-				"value": "http://natura.com.br"
-			}]
-		}, {
-			"title": "LANCAMENTO 2",
-			"subtitle": "Você paga R$ 450 em 2x no boleto e pode Vender por até R$ 585!",
-			"images": [
-				{ "url": "https://dummyimage.com/600x300" }
-			],
-			"buttons": [{
-				"type": "openUrl",
-				"title": "Veja mais sobre essa promoção",
-				"value": "http://natura.com.br"
-			}]
-		}]);
+		s.sendTyping();
+		let apiResult = await got('http://40.71.226.49/rec/sortOffers?store=natura&field=price&q=%7B%22metadata%22%3A%7B%22natura%22%3A%22Lan%C3%A7amento%22%7D%7D', {
+			headers: { 'Authorization': 'adcfdecda123491231afddaee' }
+		});
+
+		let arrayToSend = [];
+		JSON.parse(apiResult.body).forEach((thisPresente) => {
+			let precoPromocao = (thisPresente.price * 0.7).toFixed(2);
+			let prontos = Math.ceil(thisPresente.price / 4.2).toFixed(0);
+			subtitle = `De: R$ ${thisPresente.list_price.toFixed(2)} Por: R$ ${precoPromocao}`;
+
+			arrayToSend.push({
+	            title: `${thisPresente.name} (${prontos} pontos)`,
+	            subtitle: subtitle,
+	            images: [ { url: thisPresente.img } ],
+	            buttons: [{
+	                type: "postBack",
+	                title: "Adicionar ao pedido",
+	                value: "INTENT_PROMOCAO_ADD"
+	            }]
+	        });
+		});
+
+		helper.sendSlider(s, arrayToSend);
+		s.endDialog();
 	},
 
 	presente(s) {
